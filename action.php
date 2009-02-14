d0a907ab087e29242d2a126de846c8d7073ee1dd
Mykola Ostrovskyy <spambox03@mail.ru>
2009-02-14 14:22:52 +0000

    r62
    + Syntax plugin
    + Replacement of comments by HTML

--- action.php
+++ action.php
@@ -13,7 +13,7 @@ if(!defined('DOKU_INC')) die();
 if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC . 'lib/plugins/');
 require_once(DOKU_PLUGIN . 'action.php');
 
-class action_plugin_refnotes extends DokuWiki_Action_Plugin {
+class action_plugin_tablewidth extends DokuWiki_Action_Plugin {
 
     /**
      * Return some info
@@ -22,7 +22,7 @@ class action_plugin_refnotes extends DokuWiki_Action_Plugin {
         return array(
             'author' => 'Mykola Ostrovskyy',
             'email'  => 'spambox03@mail.ru',
-            'date'   => '2009-02-11',
+            'date'   => '2009-02-14',
             'name'   => 'Table Width',
             'desc'   => 'Allows to specify width of table columns.',
             'url'    => 'http://code.google.com/p/dwp-forge/',
@@ -37,14 +37,71 @@ class action_plugin_refnotes extends DokuWiki_Action_Plugin {
     }
 
     /**
-     * Replaces table-width comments by <col> tags
+     * Replace table-width comments by HTML
      */
     function replaceComments(&$event, $param) {
         if ($event->data[0] == 'xhtml') {
-            $pattern = '/(<!-- table-width [^\n]+? -->)(\s*<table.*?>\s*)(<)/';
+            $pattern = '/(<!-- table-width [^\n]+? -->\n)(\s*<table.*?>)(\s*<t)/';
             $flags = PREG_SET_ORDER | PREG_OFFSET_CAPTURE;
-            if (preg_match($pattern, $event->data[0] $match, $flags) > 0) {
+            if (preg_match_all($pattern, $event->data[1], $match, $flags) > 0) {
+                $start = 0;
+                $html = '';
+                foreach ($match as $data) {
+                    $html .= substr($event->data[1], $start, $data[0][1] - $start);
+                    $html .= $this->_processTable($data);
+                    $start = $data[0][1] + strlen($data[0][0]);
+                }
+                $event->data[1] = $html . substr($event->data[1], $start);;
             }
         }
+    }
+
+    /**
+     * Convert table-width comments and table mark-up into final HTML
+     */
+    function _processTable($data) {
+        preg_match('/<!-- table-width ([^\n]+?) -->/', $data[1][0], $match);
+        $width = preg_split('/\s+/', $match[1]);
+        $tableWidth = array_shift($width);
+        if ($tableWidth != '-') {
+            $table = $this->_styleTable($data[2][0], $tableWidth);
+        }
+        else {
+            $table = $data[2][0];
+        }
+        return $table . $this->_renderColumns($width) . $data[3][0];
+    }
+
+    /**
+     * Add width style to the table
+     */
+    function _styleTable($html, $width) {
+        preg_match('/^(\s*<table)(.*?)(>)$/', $html, $match);
+        $entry = $match[1];
+        $attributes = $match[2];
+        $exit = $match[3];
+        if (preg_match('/(.*?style\s*=\s*(["\']).*?)(\2.*)/', $attributes, $match) == 1) {
+            $attributes = $match[1] . ';width: ' . $width . ';' . $match[3];
+        }
+        else {
+            $attributes .= ' style="width: ' . $width . ';"';
+        }
+        return $entry . $attributes . $exit;
+    }
+
+    /**
+     * Render column tags
+     */
+    function _renderColumns($width) {
+        $html = DOKU_LF;
+        foreach ($width as $w) {
+            if ($w != '-') {
+                $html .= '<col style="width: ' . $w . '" />';
+            }
+            else {
+                $html .= '<col />';
+            }
+        }
+        return $html;
     }
 }
