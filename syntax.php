47587b46d3a56c70b40f42cad93574c6e7c3b387
Mykola Ostrovskyy <spambox03@mail.ru>
2022-08-09 03:01:55 +0300

    Rely on class auto-loader for dependencies

--- syntax.php
+++ syntax.php
@@ -7,12 +7,6 @@
  * @author     Mykola Ostrovskyy <spambox03@mail.ru>
  */
 
-/* Must be run within Dokuwiki */
-if(!defined('DOKU_INC')) die();
-
-if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC . 'lib/plugins/');
-require_once(DOKU_PLUGIN . 'syntax.php');
-
 class syntax_plugin_tablewidth extends DokuWiki_Syntax_Plugin {
 
     private $mode;
